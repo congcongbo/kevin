05469008a9ade1629c92fb7af67da47cdb38a921
Kim SJ <kim@spencejones.com>
2018-03-12 21:15:20 +0000

    Initial commit of Kim's code

---
+++ barcode.py
@@ -0,0 +1,36 @@
+# python barcode processing
+# run wuth zbarcode, as follows:
+
+#   zbarcam -S*.disable -Sqrcode.enable | python barcode.py
+
+
+
+
+import sys
+
+from nbstreamreader import NonBlockingStreamReader as NBSR
+nbsr = NBSR(sys.stdin)
+
+# functions to handle each case
+def altitude():
+    print "altitude"
+
+def attraction():
+    print "atraction"
+
+def unknownQR():
+    print "oops"
+
+switcher = {
+    "QR-Code:altitude": altitude,
+    "QR-Code:attraction": attraction
+}
+
+while True:
+    line=nbsr.readline(0.1)
+    if not line:
+        print ".",
+        sys.stdout.flush()
+    else:
+        switcher.get(line.strip(), unknownQR)()
+
